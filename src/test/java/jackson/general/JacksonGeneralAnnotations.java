daf6c8c7b4cefa687546f7a84715a813d18fe0e1
nkudinov <oracle_developer@mail.ru>
2018-08-07 13:12:23 +0300

    i added some general annotations

--- src/test/java/jackson/general/JacksonGeneralAnnotations.java
+++ src/test/java/jackson/general/JacksonGeneralAnnotations.java
@@ -1,10 +1,14 @@
 package jackson.general;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.BeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -53,6 +57,52 @@ public class JacksonGeneralAnnotations {
             return date;
         }
     }
+    static class A3{
+        public A3(int age, Name name) {
+            this.age = age;
+            this.internalName = name;
+        }
+
+        public int age;
+        @JsonUnwrapped
+        public Name internalName;
+        public static class Name{
+            public String firstName;
+            public String lastName;
+
+            public Name(String firstName, String lastName) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+        }
+    }
+
+    static class A4 {
+
+        public int id;
+
+        @JsonView(Views.Public.class)
+        public String itemName;
+
+        @JsonView(Views.Internal.class)
+        public String ownerName;
+
+        public A4(int id, String itemName, String ownerName) {
+            this.id = id;
+            this.itemName = itemName;
+            this.ownerName = ownerName;
+        }
+    }
+    @JsonFilter("myFilter")
+    static class A5{
+        public int age;
+        public String name;
+
+        public A5(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
     @Test
     void JsonPropertyTest() throws IOException {
         A1 a1 = new A1("John");
@@ -71,5 +121,48 @@ public class JacksonGeneralAnnotations {
         String json = mapper.writeValueAsString(a2);
         System.out.println(json);
         //Assertions.assertTrue(json1.contains("firstName"));
+    }
+
+    @Test
+    void JsonUnwrappedTest() throws JsonProcessingException {
+       A3.Name john = new A3.Name("John","Smith");
+       A3 a3 = new A3(23,john);
+       String json = mapper.writeValueAsString(a3);
+        System.out.println(json);
+       Assertions.assertFalse(json.contains("internalName"));
+    }
+    @Test
+    void JsonViewTest1() throws JsonProcessingException {
+        A4 a4 = new A4(2, "book", "John");
+        mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+        String json = mapper.writerWithView(Views.Public.class).writeValueAsString(a4);
+        Assertions.assertFalse(json.contains("id"));
+        Assertions.assertFalse(json.contains("ownerName"));
+    }
+    @Test
+    void JsonViewTest2() throws JsonProcessingException {
+        A4 a4 = new A4(2, "book", "John");
+
+        String json = mapper.writerWithView(Views.Public.class).writeValueAsString(a4);
+        System.out.println(json);
+        Assertions.assertTrue(json.contains("id"));
+        Assertions.assertTrue(json.contains("itemName"));
+        Assertions.assertFalse(json.contains("ownerName"));
+    }
+    @Test
+    void JsonViewTest3() throws JsonProcessingException {
+        A4 a4 = new A4(2, "book", "John");
+
+        String json = mapper.writerWithView(Views.Internal.class).writeValueAsString(a4);
+        Assertions.assertTrue(json.contains("id"));
+        Assertions.assertTrue(json.contains("itemName"));
+        Assertions.assertTrue(json.contains("ownerName"));
+    }
+    @Test
+    void JsonFilterTest() throws JsonProcessingException {
+        A5 a5 = new A5(23,"John");
+        FilterProvider filter = new SimpleFilterProvider().addFilter("myFilter",  SimpleBeanPropertyFilter.filterOutAllExcept("age"));
+        String json = mapper.writer(filter).writeValueAsString(a5);
+        Assertions.assertFalse(json.contains("John"));
     }
 }
---
+++ src/test/java/jackson/general/Views.java
@@ -0,0 +1,7 @@
+package jackson.general;
+
+public class Views {
+    public static class Public {}
+    public static class Internal extends Public {}
+
+}
