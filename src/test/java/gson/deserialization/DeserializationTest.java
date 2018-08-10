b514f3a1f2babc4fc0721c74bf66e2e22db2037e
nkudinov <oracle_developer@mail.ru>
2018-08-10 11:14:50 +0300

    simple Gson deserializar

--- src/test/java/gson/deserialization/DeserializationTest.java
+++ src/test/java/gson/deserialization/DeserializationTest.java
@@ -1,8 +1,10 @@
 package gson.deserialization;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Type;
 
 public class DeserializationTest {
  static class A{
@@ -22,6 +24,15 @@ public class DeserializationTest {
          return age;
      }
  }
+ static class AJsonDeserializer implements JsonDeserializer<A> {
+     @Override
+     public A deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+         JsonObject jsonObject = jsonElement.getAsJsonObject();
+         String name = jsonObject.get("n-a-m-e").getAsString();
+         int age     = jsonObject.get("a-g-e").getAsInt();
+         return new A(name,age);
+     }
+ }
  @Test
  void stringDeserializationTest(){
    Gson gson = new Gson();
@@ -35,5 +46,16 @@ public class DeserializationTest {
     A a  = gson.fromJson(json,A.class);
     Assertions.assertEquals(a.getAge(),23);
     Assertions.assertEquals(a.getName(),"John");
+ }
+ @Test
+  void simpleJsonDeserializerTest(){
+    GsonBuilder gsonBuilder = new GsonBuilder();
+    gsonBuilder.registerTypeAdapter(A.class,new AJsonDeserializer());
+    String json = "{\"n-a-m-e\":\"John\",\"a-g-e\":23}";
+    Gson gson = gsonBuilder.create();
+    A a  = gson.fromJson(json,A.class);
+    Assertions.assertEquals(a.getAge(),23);
+    Assertions.assertEquals(a.getName(),"John");
+
  }
 }
